63fbb0245c279a2bcd1aadca1b31fa69af9c1117
KhanKaif00 <khanmohdkaif780@gmail.com>
2025-02-19 22:37:49 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+
+export default app;
