0e13b2f5f9db53a9fb031d9064ecfb30eb9e37e4
KhanKaif00 <khanmohdkaif780@gmail.com>
2025-02-19 23:49:03 +0530

    m

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import connect from "./db/db.js"
+import userRoutes from './routes/user.routes.js'
 
 const app = express();
 
+connect();
+
+
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+
+app.use('/users',userRoutes)
 
 
 export default app;
